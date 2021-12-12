b5aa3efd29cfb2920bd6c1e698fa020e8a572fe6
Marcos Michel Luciano <marcos.luciano@sc.senai.br>
2021-12-12 16:21:36 -0300

    Criada classe transações

--- src/BancoTechDive/TransacaoBancaria.java
+++ src/BancoTechDive/TransacaoBancaria.java
@@ -1,4 +1,76 @@
 package BancoTechDive;
 
+import java.util.Date;
+
 public class TransacaoBancaria {
+    private Conta contaT; // conta a qual esta transação pertence
+    private int idT; // identifica unicamente esta transação
+    private Date dataT; // data da transação
+    private String historicoT; // descrição da transação
+    private double valorT;
+    private char letraT; // tipo da conta
+    public static int serialTransacao =0;
+
+    public TransacaoBancaria() {
+    }
+
+    public TransacaoBancaria(Conta contaT, int idT, Date dataT, String historicoT, double valorT, char letraT) {
+        this.contaT = contaT;
+        this.idT = idT;
+        this.dataT = dataT;
+        this.historicoT = historicoT;
+        this.valorT = valorT;
+        this.letraT = letraT;
+    }
+
+    // getter e setter
+
+
+    public Conta getContaT() {
+        return contaT;
+    }
+
+    public void setContaT(Conta contaT) {
+        this.contaT = contaT;
+    }
+
+    public int getIdT() {
+        return idT;
+    }
+
+    public void setIdT(int idT) {
+        this.idT = idT;
+    }
+
+    public Date getDataT() {
+        return dataT;
+    }
+
+    public void setDataT(Date dataT) {
+        this.dataT = dataT;
+    }
+
+    public String getHistoricoT() {
+        return historicoT;
+    }
+
+    public void setHistoricoT(String historicoT) {
+        this.historicoT = historicoT;
+    }
+
+    public double getValorT() {
+        return valorT;
+    }
+
+    public void setValorT(double valorT) {
+        this.valorT = valorT;
+    }
+
+    public char getLetraT() {
+        return letraT;
+    }
+
+    public void setLetraT(char letraT) {
+        this.letraT = letraT;
+    }
 }
